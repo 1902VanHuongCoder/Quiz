32fe797dd5f9419998a220001ca613bc0318be85
van huong <tovanhuong007@gmail.com>
2023-04-25 00:41:48 +0700

    second commit

---
+++ src/data.js
@@ -0,0 +1,57 @@
+export const questions = [
+  {
+    id: 1,
+    question: "What is the capital of France?",
+    answers: [
+      { id: "a", text: "London" },
+      { id: "b", text: "Paris" },
+      { id: "c", text: "Berlin" },
+      { id: "d", text: "Madrid" },
+    ],
+    correctAnswer: "b",
+  },
+  {
+    id: 2,
+    question: "What is the largest country in the world?",
+    answers: [
+      { id: "a", text: "Russia" },
+      { id: "b", text: "China" },
+      { id: "c", text: "USA" },
+      { id: "d", text: "India" },
+    ],
+    correctAnswer: "a",
+  },
+  {
+    id: 3,
+    question: "What is the currency of Japan?",
+    answers: [
+      { id: "a", text: "Yen" },
+      { id: "b", text: "Dollar" },
+      { id: "c", text: "Euro" },
+      { id: "d", text: "Pound" },
+    ],
+    correctAnswer: "a",
+  },
+  {
+    id: 4,
+    question: "What is the capital of France?",
+    answers: [
+      { id: "a", text: "London" },
+      { id: "b", text: "Paris" },
+      { id: "c", text: "Berlin" },
+      { id: "d", text: "Madrid" },
+    ],
+    correctAnswer: "c",
+  },
+  {
+    id: 5,
+    question: "What is the currency of Japan?",
+    answers: [
+      { id: "a", text: "Yen" },
+      { id: "b", text: "Dollar" },
+      { id: "c", text: "Euro" },
+      { id: "d", text: "Pound" },
+    ],
+    correctAnswer: "d",
+  }
+];
