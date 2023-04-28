62672d2352d5a11784370dc8ad3719227ba8fa90
van huong <tovanhuong007@gmail.com>
2023-04-28 22:56:02 +0700

    commit 28/04/2023

--- src/App.js
+++ src/App.js
@@ -10,25 +10,255 @@ import { questions } from "./data";
 import { useState } from "react";
 import "./App.css";
 import logo from "./logo.png";
+
+/************************* Use Material Libary *********************/
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { SignLanguage } from "@mui/icons-material";
+
 export default function Ungdung() {
   const [isHome, setIsHome] = useState(true);
   const [isTest, setTest] = useState(false);
-  const [input, setInput] = useState([]);
+  const [isLogin, setIsLogin] = useState(true);
+  const [isSignup, setIsSignUp] = useState(false);
   return (
     <div className="container">
-      <Navigation setIsHomeAr={setIsHome} setIsTest={setTest} />
-      <Sidebar />
-      {isTest === true && <Test />}
-      {isHome === true && (
-        <InforMyWeb setIsTest={setTest} setIsHomeAr={setIsHome} />
+      {isLogin && <LogIn setIsLogin={setIsLogin} setIsSignUp={setIsSignUp} />}
+      {isSignup && isLogin === false && (
+        <SignUp setIsLogin={setIsLogin} setIsSignUp={setIsSignUp} />
       )}
-      {isTest === false && isHome === false && (
-        <Enterquestion input={input} setInput={setInput} />
+      {isSignup === false && isLogin === false && (
+        <>
+          <Navigation
+            setIsHomeAr={setIsHome}
+            setIsTest={setTest}
+            setIsSignUp={setIsSignUp}
+            setIsLogin={setIsLogin}
+          />
+          <Sidebar />
+          {isTest === true && <Test />}
+          {isHome === true && (
+            <InforMyWeb setIsTest={setTest} setIsHomeAr={setIsHome} />
+          )}
+          {isTest === false && isHome === false && <Enterquestion />}
+          <Footer />
+        </>
       )}
-      <Footer />
     </div>
   );
 }
+
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Paul To Web
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
+function LogIn({ setIsSignUp, setIsLogin }) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const inputLogin = {
+      email: data.get("email"),
+      password: data.get("password")
+    };
+    console.log(inputLogin);
+    const localInfo = JSON.parse(localStorage.getItem("signupInfo"));
+    if (
+      inputLogin.email === localInfo.email &&
+      inputLogin.password === localInfo.password
+    ) {
+      setIsLogin(false);
+      setIsSignUp(false);
+    }
+    console.log(localInfo.email);
+  };
+  const handleSignUp = () => {
+    setIsLogin(false);
+    setIsSignUp(true);
+  };
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Log in
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Log in
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2" onClick={handleSignUp}>
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+/***************** FORM SIGN UP ***********************/
+function SignUp({ setIsLogin, setIsSignUp }) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const infoUser = {
+      email: data.get("email"),
+      password: data.get("password"),
+    };
+    const localInfo = JSON.parse(localStorage.getItem("signupInfo"));
+    localInfo.email === infoUser.email
+      ? alert("Xin loi! Tai khoan nay da duoc su dung")
+      : setIsLogin(false);
+    setIsSignUp(false);
+    localStorage.setItem("signupInfo", JSON.stringify(infoUser));
+  };
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign up
+            </Button>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
+/******************************************************** */
+
 export function Enterquestion({ input, setInput }) {
   const inputObject = {};
   const handleInput = (e) => {
@@ -120,7 +350,12 @@ export function InforMyWeb({ setIsTest, setIsHomeAr }) {
     </div>
   );
 }
-export function Navigation({ setIsHomeAr, setIsTest }) {
+export function Navigation({
+  setIsHomeAr,
+  setIsTest,
+  setIsSignUp,
+  setIsLogin,
+}) {
   let handleConvertToHome = () => {
     setIsHomeAr(true);
     setIsTest(false);
@@ -133,11 +368,16 @@ export function Navigation({ setIsHomeAr, setIsTest }) {
     setIsTest(false);
     setIsHomeAr(false);
   };
-  let handleToggleMenu = () =>
-  {
-      let toggleMenu = document.querySelector('.nav-toggle-menu');
-      toggleMenu.classList.toggle('open');
-  }
+  const handleSignUp = () => {
+    setIsSignUp(true);
+  };
+  const handleLogin = () => {
+    setIsLogin(true);
+  };
+  let handleToggleMenu = () => {
+    let toggleMenu = document.querySelector(".nav-toggle-menu");
+    toggleMenu.classList.toggle("open");
+  };
   return (
     <header className="nav">
       <div id="nav-logo">
@@ -151,10 +391,14 @@ export function Navigation({ setIsHomeAr, setIsTest }) {
         <li>Phản Hồi</li>
       </ul>
       <div className="nav-login-signin">
-        <button>Đăng Ký</button>
-        <button>Đăng Nhập</button>
+        <button onClick={handleSignUp}>Đăng Ký</button>
+        <button onClick={handleLogin}>Đăng Nhập</button>
       </div>
-      <FontAwesomeIcon className="nav-bars-icon" icon={faBars} onClick={handleToggleMenu}/>
+      <FontAwesomeIcon
+        className="nav-bars-icon"
+        icon={faBars}
+        onClick={handleToggleMenu}
+      />
       <div className="nav-toggle-menu">
         <ul>
           <li onClick={handleConvertToHome}>
